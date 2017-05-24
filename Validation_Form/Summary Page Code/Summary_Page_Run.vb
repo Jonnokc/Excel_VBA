ae3db7a5afebfd7ba9a27ed716e8d757efc1fb83
Jonnokc <JA052464@cerner.net>
2017-05-24 10:49:01 -0500

    adjusted script order

--- Validation_Form/Summary Page Code/Summary_Page_Run.vb
+++ Validation_Form/Summary Page Code/Summary_Page_Run.vb
@@ -582,33 +582,6 @@ Private Sub Summary_Pop_Dots()
 End Sub
 
 
-
-Sub Summary_Sheet_Setup()
-
-'Prompts user to confirm they have reviewed the data in the validation form BEFORE running this.
-    Confirm_Scrubbed = MsgBox("You have initiated the program to initalize the Summary Sheet. Please click ""Ok"" to run or ""Cancel"" to close the program", vbOKCancel + vbQuestion, "Empty Sheet")
-
-    'If user hits cancel then close program.
-    If Confirm_Scrubbed = vbCancel Then
-        MsgBox ("Program is canceling per user action.")
-        Exit Sub
-
-    End If
-
-
-    Call Summary_Create_Lookup_Sheet
-    Call Summary_Combined_Lookup_Sheet
-    Call Summary_Sheet_Initial_Setup
-    Call Summary_Pop_Dots
-    Call Summary_Cleanup
-    Call Remove_Table_Format
-
-    Sheets("Summary View").Select
-
-    MsgBox ("Program Completed")
-
-End Sub
-
 Private Sub Summary_Sheet_Initial_Setup()
 '
 ' Sets up the Summary Sheet. Copies the Registries and Measures and creates the concat column
@@ -794,5 +767,31 @@ ClearError:
         'Clears variables for next loop
 
     Next i
+
+End Sub
+
+Sub Summary_Sheet_Setup()
+
+'Prompts user to confirm they have reviewed the data in the validation form BEFORE running this.
+    Confirm_Scrubbed = MsgBox("You have initiated the program to initalize the Summary Sheet. Please click ""Ok"" to run or ""Cancel"" to close the program", vbOKCancel + vbQuestion, "Empty Sheet")
+
+    'If user hits cancel then close program.
+    If Confirm_Scrubbed = vbCancel Then
+        MsgBox ("Program is canceling per user action.")
+        Exit Sub
+
+    End If
+
+
+    Call Summary_Create_Lookup_Sheet
+    Call Summary_Combined_Lookup_Sheet
+    Call Summary_Sheet_Initial_Setup
+    Call Summary_Pop_Dots
+    Call Summary_Cleanup
+    Call Remove_Table_Format
+
+    Sheets("Summary View").Select
+
+    MsgBox ("Program Completed")
 
 End Sub
